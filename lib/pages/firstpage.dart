d25c639279f45211ea9269d7d67f3fc5308bc95f
Nattrareya <662021049@tsu.ac.th>
2025-12-18 17:12:44 +0700

    first commit

---
+++ lib/pages/firstpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Firstpage extends StatelessWidget {
+  const Firstpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("profile Nattareya"),
+      ),
+      
+    );
+  }
+}
